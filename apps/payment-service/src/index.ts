ce9d14a77cbc4160a08d66b3845fd9fb99b8efac
HyperDanisH <hypertxt.assin@gmail.com>
2025-12-06 09:52:54 +0530

    payment-service auth middleware

--- apps/payment-service/src/index.ts
+++ apps/payment-service/src/index.ts
@@ -1,11 +1,12 @@
 import { clerkMiddleware } from '@hono/clerk-auth'
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { shouldBeUser } from './middleware/authMiddleware.js'
 
 const app = new Hono()
 app.use('*', clerkMiddleware())
 
-app.get('/', (c) => {
+app.get('/', shouldBeUser, (c) => {
   return c.text('Payment service is up and running!')
 })
 
